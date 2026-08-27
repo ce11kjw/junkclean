71f20d7e837e650af3a16d75cdbaac38d50b8359
ce11kjw <ce11kjw@local>
2026-08-27 00:48:31 +0000

    release v4.2.2 (versionCode 422)

--- main.go
+++ main.go
@@ -26,8 +26,8 @@ import (
 var webFS embed.FS
 
 const (
-	ver      = "4.2.1"
-	verCode  = 421
+	ver      = "4.2.2"
+	verCode  = 422
 	port     = "46780"
 	stateDir = "/data/adb/junkclean"
 	logFile  = stateDir + "/junkclean.log"
