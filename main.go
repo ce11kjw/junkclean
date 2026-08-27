dbdd76428fda307a7289044e32ca09fb0654129f
ce11kjw <ce11kjw@local>
2026-08-27 14:47:42 +0000

    release v4.2.20 (versionCode 440)

--- main.go
+++ main.go
@@ -26,8 +26,8 @@ import (
 var webFS embed.FS
 
 const (
-	ver      = "4.2.19"
-	verCode  = 439
+	ver      = "4.2.20"
+	verCode  = 440
 	port     = "46780"
 	stateDir = "/data/adb/junkclean"
 	logFile  = stateDir + "/junkclean.log"
